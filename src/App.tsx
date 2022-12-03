19de404eb0d8bb9c6f5d42a504b24f6cb5b52369
Jose Hernandez <josemanuelhdez2@gmail.com>
2022-12-03 18:21:03 +0000

    Update 5 files. sky bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import "./styles.css";
+import styled from "styled-components";
+
+const SkyBg = styled.div`
+  background: rgb(2,0,36);
+  background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(7,7,93,1) 47%, rgba(29,55,150,1) 77%, rgba(150,211,252,1) 100%);
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainCard = styled.div`
+  border: 3px dotted orange;
+  background: palegoldenrod;
+  width: 50rem;
+  margin: auto;
+`;
 
 export default function App() {
   return (
     <div className="App">
-      <h1>🇯🇵 Hello Japanki 🇯🇵 </h1>
-      {["Anki", "Koohii", "Kanji Study"].map((e) => (
-        <h2>{e}</h2>
-      ))}
+      <SkyBg>
+        <MainCard>
+          <h1>🇯🇵 Hello Japanki 🇯🇵 </h1>
+          {["Anki", "Koohii", "Kanji Study"].map((e) => (
+            <h2>{e}</h2>
+          ))}
+        </MainCard>
+      </SkyBg>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { styled } from "styled-components";
 
 import App from "./App";
 
